82e507d7f2bd65d553fb205dcaeb5b2b3086a038
Sahil Silare <sahilsilare@gmail.com>
2024-08-30 23:51:13 +0530

    feat: adding gh-pages to package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
