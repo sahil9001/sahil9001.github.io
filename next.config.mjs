35115dcffb1a28e7f0f0122c52a073827a7327c6
Sahil Silare <ssilare@adobe.com>
2025-07-12 17:29:17 +0530

    feat: added new workflow
    Signed-off-by: Sahil Silare <ssilare@adobe.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,13 @@
  * @type {import('next').NextConfig}
  * This config ensures correct asset paths for GitHub Pages static export.
  */
-const repo = 'sahil9001.github.io';
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: "export",
     images: {
       unoptimized: true,
     },
-    assetPrefix: isProd ? `/${repo}` : '',
-    basePath: isProd ? `/${repo}` : '',
+    assetPrefix: 'sahil9001.github.io',
+    basePath: 'sahil9001.github.io',
 };
 
 export default nextConfig;
